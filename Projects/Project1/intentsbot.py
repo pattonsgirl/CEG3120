8268ea49702f308cf768a401e94d38c8ac912d83
pattonsgirl <pattonsgirl@gmail.com>
2022-09-12 11:38:46 -0400

    works with discord.py 2.0.1

---
+++ Projects/Project1/intentsbot.py
@@ -0,0 +1,46 @@
+# Based on example https://discordpy.readthedocs.io/en/stable/quickstart.html
+import os
+
+import discord
+import random
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    hitchhiker_quotes = [
+        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
+        'It is a mistake to think you can solve any major problems just with potatoes.',
+        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
+        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
+    ]
+    if message.content == 'towel!':
+    #if message.content.startswith('$towel'):
+        response = random.choice(hitchhiker_quotes)
+        await message.channel.send(response)
+
+client.run(TOKEN)
