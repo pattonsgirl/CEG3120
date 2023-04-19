2c5d501f3187ba413c104485e110b44e32da4889
Kayleigh Duncanwq <pattonsgirl@gmail.com>
2023-04-18 21:33:08 -0400

    updates

--- CourseNotes/books-api/api.py
+++ CourseNotes/books-api/api.py
@@ -23,6 +23,22 @@ def all_books():
 
 @app.route('/api/v1/resources/books/newbook', methods=['GET', 'POST'])
 def new_book():
+    # when GET, return form.  when POST, process data
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
     conn = get_db_connection()
     #books = conn.execute('SELECT * FROM books').fetchall()
     
